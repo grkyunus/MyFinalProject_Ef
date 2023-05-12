2f24f50734de09e79ba002b8e9c6caccba228535
Gırık <yunus.yk2000@gmail.com>
2023-05-12 19:44:46 +0300

    10.gün

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün İsmi geçersiz";
-
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductListed = "Ürünler listelendi";
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleUI
         private static void Main(string[] args)
         {
             // Data Transformation Object
-             ProductTest();
+            ProductTest();
             // IoC araştırılabilir.
             // CategoryTest();
 
@@ -31,10 +31,19 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+            if (result.Success == true)
             {
-                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                foreach (var product in productManager.GetProductDetails().Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
         }
     }
 }
