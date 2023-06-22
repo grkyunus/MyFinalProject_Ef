22a6caf95e9de55bf8491faadc557878086e235d
Gırık <yunus.yk2000@gmail.com>
2023-06-22 08:27:03 +0300

    backend and frontend connection and product controller sleep test

--- WebAPI/Controller/ProductsController.cs
+++ WebAPI/Controller/ProductsController.cs
@@ -26,6 +26,8 @@ namespace WebAPI.Controller
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
+            Thread.Sleep(5000);
+
             var result = _productService.GetAll();
             if (result.Success)
             {
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -33,7 +33,7 @@ builder.Services.AddControllers();
 
 // builder.Services.AddHttpContextAccessor();
 
-
+builder.Services.AddCors();
 
 var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -72,6 +72,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseCors(builder=>builder.WithOrigins("http://localhost:60972").AllowAnyHeader());
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
